23f024e205e73c47451e9dc46970978870bbfeb8
unknown <cameronwfinney@gmail.com>
2022-10-08 20:38:43 +1300

    change from light to dark mode

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-function swapStyleSheet(sheet) {}
+function swapStyleSheet() {
+  const linkPageStyle = document.getElementById("pagestyle");
+  let currentStyle = linkPageStyle.getAttribute("href");
+  if ((currentStyle = "darkStyle.css")) {
+    linkPageStyle.setAttribute("href", "style.css");
+    currentStyle = "style.css";
+  } else if ((currentStyle = "style.css")) {
+    linkPageStyle.setAttribute("href", "darkStyle.css");
+    currentStyle = "darkStyle.css";
+  }
+  console.log(currentStyle);
+}
+
+let toggleButton = document.getElementById("button");
+
+toggleButton.addEventListener("click", swapStyleSheet);
