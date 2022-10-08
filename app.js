eac79c5499a63404d510eb62f2a9a96706cb7592
unknown <cameronwfinney@gmail.com>
2022-10-08 20:22:57 +1300

    add dark mode styling sheet

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-// dark mode function
-function darkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-  console.log("Button pressed");
-}
-
-document.getElementById("button").addEventListener("click", darkMode);
+function swapStyleSheet(sheet) {}
